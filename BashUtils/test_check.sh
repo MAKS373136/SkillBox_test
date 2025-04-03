d260f6bedc902fda380c372c0a95d390b84484fa
maks <sapemaksim@ya.ru>
2025-04-03 22:54:10 +0300

    style_check

---
+++ BashUtils/test_check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd cat/
+
+bash test_cat.sh &> res_cat.txt
+
+RES_C=$(grep -c "FAIL" res_cat.txt)
+if [[ $RES_C -ne 0 ]]; then
+    exit 1
+fi
+
+rm -rf res_cat.txt
+
+cd ../grep
+ 
+bash test_grep.sh &> res_grep.txt
+
+RES_G=$(grep -c "FAIL" res_grep.txt)
+if [[ $RES_G -ne 0 ]]; then
+    exit 1
+fi
+
+rm -rf res_grep.txt
